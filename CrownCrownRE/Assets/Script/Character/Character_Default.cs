a496e82c947722e73bf25210ee254ebfd901d8d1
김종근 <husky@gimjong-geun-ui-Mac-mini.local>
2017-11-25 16:22:34 +0900

    Project 생성

---
+++ CrownCrownRE/Assets/Script/Character/Character_Default.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TCoin{
+public class Character_Default : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+}
