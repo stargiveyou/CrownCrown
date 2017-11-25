746e8681e7413b04e4b12a05a22a96ebb0b9bd42
김종근 <husky@gimjong-geun-ui-Mac-mini.local>
2017-11-25 17:03:00 +0900

    default Character 함수 제작 중

--- CrownCrownRE/Assets/Script/Character/Character_Default.cs
+++ CrownCrownRE/Assets/Script/Character/Character_Default.cs
@@ -3,18 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace TCoin{
+namespace TCoin
+{
+
+    public enum CharacterType
+    {
+        None,
+    }
+
+
+    //Character ---> Monster, PlayerCharacter, NPC ETC
 public class Character_Default : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+        #region Static Temp Method
+
+        public static Character_Default CreateDefaultInstance(string name)
+        {
+            Character_Default newCharacter = Instantiate(new GameObject(name), Vector3.zero, Quaternion.identity).AddComponent<Character_Default>();
+
+            newCharacter.CreateCharacter(name, 0);
+
+            return newCharacter;
+        }
+
+        #endregion
+
+        public virtual CharacterType CharacterType
+        {
+            get
+            {
+                return CharacterType.None;
+            }
+        }
+
+        public string characterName;
+        public int characterid;
+        public float hp;
+        public float speed;
+
+
+        // Use this for initialization
+        void Start () {
+        	
+        }
+
+#region Character Create and Remove
+        public virtual void CreateCharacter(string Name, int id, float hp, float speed)
+        {
+            
+        }
+        public virtual void CreateCharacter(string Name, int id)
+        {
+            
+        }
+        public virtual void RemoveCharacter(string name)
+        {
+            
+        }
+        #endregion
+
+        #region Character Movement
+
+
+        public void MoveToForward()
+        {
+            
+        }
+
+        public void RotateAxis(float axisValue)
+        {
+            
+        }
+
+        public void Jump()
+        {
+            
+        }
+
+
+
+        #endregion
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+   }
 }
 
-}
+
+
